63f50819b91686c0b1ac36d43b1cdfd6bb6ee5c3
Mohammad Allam <uodeeb@gmail.com>
2022-04-25 22:18:23 +0200

    Finishin all the project requirements

--- starter/src/assets/front.js
+++ starter/src/assets/front.js
@@ -1,12 +1,11 @@
-let currencySymbol = '$';
-
+let currencySymbol = "$";
 // Draws product list
 function drawProducts() {
-    
-    let productList = document.querySelector('.products');
-    let productItems = '';
-    productsArr.forEach(element => {
-        productItems +=`
+  let productList = document.querySelector(".products");
+
+  let productItems = "";
+  productsArr.forEach((element) => {
+    productItems += `
             <div data-SKU='${element.SKU}'>
                 <img src='${element.image}'>
                 <h3>${element.name}</h3>
@@ -14,20 +13,20 @@ function drawProducts() {
                 <button class="add-to-cart">Add to Cart</button>
             </div>
         `;
-    });
-    // use innerHTML so that products only drawn once
-    productList.innerHTML = productItems;
+  });
+  // use innerHTML so that products only drawn once
+  productList.innerHTML = productItems;
 }
 
 // Draws cart
 function drawCart() {
-    let cartList = document.querySelector('.cart');
-    // clear cart before drawing
-    let cartItems = '';
-    cartArr.forEach(element => {
-        let itemTotal = element.price * element.quantity;
+  let cartList = document.querySelector(".cart");
+  // clear cart before drawing
+  let cartItems = "";
+  cartArr.forEach((element) => {
+    let itemTotal = element.price * element.quantity;
 
-        cartItems +=`
+    cartItems += `
             <div data-SKU='${element.SKU}'>
                 <h3>${element.name}</h3>
                 <p>price: ${currencySymbol}${element.price}</p>
@@ -38,22 +37,23 @@ function drawCart() {
                 <button class="remove">remove</button>
             </div>
         `;
-    });
-     // use innerHTML so that cart products only drawn once
-    cartArr.length ? cartList.innerHTML = cartItems : cartList.innerHTML = 'Cart Empty';
+  });
+  // use innerHTML so that cart products only drawn once
+  cartArr.length
+    ? (cartList.innerHTML = cartItems)
+    : (cartList.innerHTML = "Cart Empty");
 }
 
 // Draws checkout
 function drawCheckout() {
-    let checkout = document.querySelector('.cart-total');
-    checkout.innerHTML ='';
-
-    // run cartTotal() from script.js
-    let cartSum = cartTotal();
-
-    let div = document.createElement("div");
-    div.innerHTML =`<p>Cart Total: ${currencySymbol}${cartSum}`;
-    checkout.append(div);
+  let checkout = document.querySelector(".cart-total");
+  checkout.innerHTML = "";
+  // run cartTotal() from script.js
+  let cartSum = cartTotal();
+  console.log(cartSum);
+  let div = document.createElement("div");
+  div.innerHTML = `<p>Cart Total: ${currencySymbol}${cartSum}`;
+  checkout.append(div);
 }
 
 // Initialize store with products, cart, and checkout
@@ -61,134 +61,130 @@ drawProducts();
 drawCart();
 drawCheckout();
 
-document.querySelector('.products').addEventListener('click', (e) => {
-    let productSKU = e.target.parentNode.getAttribute('data-SKU');
-    productSKU *= 1;
-    addToCart(productSKU);     
-    drawCart();
-    drawCheckout();
-})
-
+document.querySelector(".products").addEventListener("click", (e) => {
+  let productSKU = e.target.parentNode.getAttribute("data-SKU");
+  productSKU *= 1;
+  addToCart(productSKU);
+  drawCart();
+  drawCheckout();
+});
 
 // Event delegation used to support dynamically added cart items
-document.querySelector('.cart').addEventListener('click', (e) => {
-    
-    // Helper nested higher order function to use below
-    // Must be nested to have access to the event target
-    // Takes in a cart function as an agrument
-    function runCartFunction(fn) {
-        let productSKU = e.target.parentNode.getAttribute('data-SKU');
-        productSKU *= 1;
-        for (let i = cartArr.length - 1; i > -1; i--) {
-            if (cartArr[i].SKU === productSKU) {
-                let SKU = cartArr[i].SKU;
-                fn(SKU);
-            }
-        }
-        // force cart and checkout redraw after cart function completes
-        drawCart();
-        drawCheckout();
+document.querySelector(".cart").addEventListener("click", (e) => {
+  // Helper nested higher order function to use below
+  // Must be nested to have access to the event target
+  // Takes in a cart function as an agrument
+  function runCartFunction(fn) {
+    let productSKU = e.target.parentNode.getAttribute("data-SKU");
+    productSKU *= 1;
+    for (let i = cartArr.length - 1; i > -1; i--) {
+      if (cartArr[i].SKU === productSKU) {
+        let SKU = cartArr[i].SKU;
+        fn(SKU);
+      }
     }
+    // force cart and checkout redraw after cart function completes
+    drawCart();
+    drawCheckout();
+  }
 
-    // check the target's class and run function based on class
-    if (e.target.classList.contains('remove')){
-        // run remove() from script.js
-        runCartFunction(remove);
-    } else if (e.target.classList.contains('qup')){
-        // run increase() from script.js
-        runCartFunction(increase);
-    } else if (e.target.classList.contains('qdown')){
-        // run decrease() from script.js
-        runCartFunction(decrease);
-    } 
-})
+  // check the target's class and run function based on class
+  if (e.target.classList.contains("remove")) {
+    // run remove() from script.js
+    runCartFunction(remove);
+  } else if (e.target.classList.contains("qup")) {
+    // run increase() from script.js
+    runCartFunction(increase);
+  } else if (e.target.classList.contains("qdown")) {
+    // run decrease() from script.js
+    runCartFunction(decrease);
+  }
+});
 
-document.querySelector('.pay').addEventListener('click', (e) => {
-    e.preventDefault();
+document.querySelector(".pay").addEventListener("click", (e) => {
+  e.preventDefault();
 
-    // Get input cash received field value, set to number
-    let amount = document.querySelector('.received').value;
-    amount *= 1;
+  // Get input cash received field value, set to number
+  let amount = document.querySelector(".received").value;
+  amount *= 1;
 
-    // Set cashReturn to return value of pay()
-    let cashReturn = pay(amount);
-    
-    let paymentSummary = document.querySelector('.pay-summary');
-    let div = document.createElement("div");
+  // Set cashReturn to return value of pay()
+  let cashReturn = pay(amount);
 
-    // If total cash received is greater than cart total thank customer
-    // Else request additional funds
-    if (cashReturn >= 0 ){
-        div.innerHTML =`
+  let paymentSummary = document.querySelector(".pay-summary");
+  let div = document.createElement("div");
+
+  // If total cash received is greater than cart total thank customer
+  // Else request additional funds
+  if (cashReturn >= 0) {
+    div.innerHTML = `
             <p>Cash Received: ${currencySymbol}${amount}</p>
             <p>Cash Returned: ${currencySymbol}${cashReturn}</p>
             <p>Thank you!</p>
         `;
-    } else {
-        // reset cash field for next entry
-        document.querySelector('.received').value = ''; 
-        div.innerHTML =`
+  } else {
+    // reset cash field for next entry
+    document.querySelector(".received").value = "";
+    div.innerHTML = `
             <p>Cash Received: ${currencySymbol}${amount}</p>
             <p>Remaining Balance: ${cashReturn}$</p>
             <p>Please pay additional amount.</p>
             <hr/>
         `;
-    }
+  }
 
-    paymentSummary.append(div);
-})
-
+  paymentSummary.append(div);
+});
 
 /* Standout suggestions */
 /* Begin remove all items from cart */
-// function dropCart(){
-//     let shoppingCart = document.querySelector('.empty-btn');
-//     let div = document.createElement("button");
-//     div.classList.add("empty");
-//     div.innerHTML =`Empty Cart`;
-//     shoppingCart.append(div);
-// }
-// dropCart();
+function dropCart(){
+    let shoppingCart = document.querySelector('.empty-btn');
+    let div = document.createElement("button");
+    div.classList.add("empty");
+    div.innerHTML =`Empty Cart`;
+    shoppingCart.append(div);
+}
+dropCart();
 
-// document.querySelector('.empty-btn').addEventListener('click', (e) => {
-//     if (e.target.classList.contains('empty')){
-//         emptyCart();
-//         drawCart();
-//         drawCheckout();
-//     }
-// })
+document.querySelector('.empty-btn').addEventListener('click', (e) => {
+    if (e.target.classList.contains('empty')){
+        emptyCart();
+        drawCart();
+        drawCheckout();
+    }
+})
 /* End all items from cart */
 
 /* Begin currency converter */
-// function currencyBuilder(){
-//     let currencyPicker = document.querySelector('.currency-selector');
-//     let select = document.createElement("select");
-//     select.classList.add("currency-select");
-//     select.innerHTML = `<option value="USD">USD</option>
-//                         <option value="EUR">EUR</option>
-//                         <option value="YEN">YEN</option>`;
-//     currencyPicker.append(select);
-// }
-// currencyBuilder();
+function currencyBuilder(){
+    let currencyPicker = document.querySelector('.currency-selector');
+    let select = document.createElement("select");
+    select.classList.add("currency-select");
+    select.innerHTML = `<option value="USD">USD</option>
+                        <option value="EUR">EUR</option>
+                        <option value="YEN">YEN</option>`;
+    currencyPicker.append(select);
+}
+currencyBuilder();
 
-// document.querySelector('.currency-select').addEventListener('change', function handleChange(event) {
-//     switch(event.target.value){
-//         case 'EUR':
-//             currencySymbol = '€';
-//             break;
-//         case 'YEN':
-//             currencySymbol = '¥';
-//             break;
-//         default:
-//             currencySymbol = '$';
-//             break;
-//      }
+document.querySelector('.currency-select').addEventListener('change', function handleChange(event) {
+    switch(event.target.value){
+        case 'EUR':
+            currencySymbol = '€';
+            break;
+        case 'YEN':
+            currencySymbol = '¥';
+            break;
+        default:
+            currencySymbol = '$';
+            break;
+     }
 
-//     currency(event.target.value);
-//     drawProducts();
-//     drawCart();
-//     drawCheckout();
-// });
+    currency(event.target.value);
+    drawProducts();
+    drawCart();
+    drawCheckout();
+});
 /* End currency converter */
 /* End standout suggestions */
-
--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,78 +1,340 @@
-/* Create 3 or more product objects using object literal notation 
-   Each product should include four properties
-   - name: name of product (string)
-   - price: price of product (number)
-   - quantity: quantity in cart (integer)
-   - SKU: unique id for product (number)
-   - image: picture of product (url string)
-*/
-
-/* Images provided in /images folder. All images from Unsplash.com
-   - cherry.jpg by Mae Mu
-   - orange.jpg by Mae Mu
-   - strawberry.jpg by Allec Gomes
-*/
-  
-
 /* Create an array named productsArr and add all product object literals to array */
-
-
+const productsArr = [
+  {
+    SKU: 1,
+    name: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: 109.95,
+    description:
+      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    rate: 3.9,
+    quantity: 0,
+    count: 50,
+  },
+  {
+    SKU: 2,
+    name: "Mens Casual Premium Slim Fit T-Shirts ",
+    price: 22.3,
+    description:
+      "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+    category: "men's clothing",
+    image:
+      "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+    rate: 4.1,
+    quantity: 0,
+    count: 70,
+  },
+  {
+    SKU: 3,
+    name: "Mens Cotton Jacket",
+    price: 55.99,
+    description:
+      "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+    rate: 4.7,
+    quantity: 0,
+    count: 30,
+  },
+  {
+    SKU: 4,
+    name: "Mens Casual Slim Fit",
+    price: 15.99,
+    description:
+      "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+    rate: 2.1,
+    quantity: 0,
+    count: 52,
+  },
+  {
+    SKU: 5,
+    name: "John Hardy Women's Legends Naga Gold & Silver Dragon Station Chain Bracelet",
+    price: 695,
+    description:
+      "From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
+    category: "jewelery",
+    image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+    rate: 4.6,
+    quantity: 0,
+    count: 141,
+  },
+  {
+    SKU: 6,
+    name: "Solid Gold Petite Micropave ",
+    price: 168,
+    description:
+      "Satisfaction Guaranteed. Return or exchange any order within 30 days.Designed and sold by Hafeez Center in the United States. Satisfaction Guaranteed. Return or exchange any order within 30 days.",
+    category: "jewelery",
+    image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
+    rate: 3.9,
+    quantity: 0,
+    count: 54,
+  },
+  {
+    SKU: 7,
+    name: "White Gold Plated Princess",
+    price: 9.99,
+    description:
+      "Classic Created Wedding Engagement Solitaire Diamond Promise Ring for Her. Gifts to spoil your love more for Engagement, Wedding, Anniversary, Valentine's Day...",
+    category: "jewelery",
+    image: "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
+    rate: 3,
+    quantity: 0,
+    count: 45,
+  },
+  {
+    SKU: 8,
+    name: "Pierced Owl Rose Gold Plated Stainless Steel Double",
+    price: 10.99,
+    description:
+      "Rose Gold Plated Double Flared Tunnel Plug Earrings. Made of 316L Stainless Steel",
+    category: "jewelery",
+    image: "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_.jpg",
+    rate: 1.9,
+    quantity: 0,
+    count: 90,
+  },
+  {
+    SKU: 9,
+    name: "WD 2TB Elements Portable External Hard Drive - USB 3.0 ",
+    price: 64,
+    description:
+      "USB 3.0 and USB 2.0 Compatibility Fast data transfers Improve PC Performance High Capacity; Compatibility Formatted NTFS for Windows 10, Windows 8.1, Windows 7; Reformatting may be required for other operating systems; Compatibility may vary depending on user’s hardware configuration and operating system",
+    category: "electronics",
+    image: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
+    rate: 3.3,
+    quantity: 0,
+    count: 12,
+  },
+  {
+    SKU: 10,
+    name: "SanDisk SSD PLUS 1TB Internal SSD - SATA III 6 Gb/s",
+    price: 109,
+    description:
+      "Easy upgrade for faster boot up, shutdown, application load and response (As compared to 5400 RPM SATA 2.5” hard drive; Based on published specifications and internal benchmarking tests using PCMark vantage scores) Boosts burst write performance, making it ideal for typical PC workloads The perfect balance of performance and reliability Read/write speeds of up to 535MB/s/450MB/s (Based on internal testing; Performance may vary depending upon drive capacity, host device, OS and application.)",
+    category: "electronics",
+    image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_.jpg",
+    rate: 2.9,
+    quantity: 0,
+    count: 7,
+  },
+  {
+    SKU: 11,
+    name: "Silicon Power 256GB SSD 3D NAND A55 SLC Cache Performance Boost SATA III 2.5",
+    price: 109,
+    description:
+      "3D NAND flash are applied to deliver high transfer speeds Remarkable transfer speeds that enable faster bootup and improved overall system performance. The advanced SLC Cache Technology allows performance boost and longer lifespan 7mm slim design suitable for Ultrabooks and Ultra-slim notebooks. Supports TRIM command, Garbage Collection technology, RAID, and ECC (Error Checking & Correction) to provide the optimized performance and enhanced reliability.",
+    category: "electronics",
+    image: "https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_.jpg",
+    rate: 4.8,
+    quantity: 0,
+    count: 56,
+  },
+  {
+    SKU: 12,
+    name: "WD 4TB Gaming Drive Works with Playstation 4 Portable External Hard Drive",
+    price: 114,
+    description:
+      "Expand your PS4 gaming experience, Play anywhere Fast and easy, setup Sleek design with high capacity, 3-year manufacturer's limited warranty",
+    category: "electronics",
+    image: "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg",
+    rate: 4.8,
+    quantity: 0,
+    count: 84,
+  },
+  {
+    SKU: 13,
+    name: "Acer SB220Q bi 21.5 inches Full HD (1920 x 1080) IPS Ultra-Thin",
+    price: 599,
+    description:
+      "21. 5 inches Full HD (1920 x 1080) widescreen IPS display And Radeon free Sync technology. No compatibility for VESA Mount Refresh Rate: 75Hz - Using HDMI port Zero-frame design | ultra-thin | 4ms response time | IPS panel Aspect ratio - 16: 9. Color Supported - 16. 7 million colors. Brightness - 250 nit Tilt angle -5 degree to 15 degree. Horizontal viewing angle-178 degree. Vertical viewing angle-178 degree 75 hertz",
+    category: "electronics",
+    image: "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_.jpg",
+    rate: 2.9,
+    quantity: 0,
+    count: 147,
+  },
+  {
+    SKU: 15,
+    name: "BIYLACLESEN Women's 3-in-1 Snowboard Jacket Winter Coats",
+    price: 56.99,
+    description:
+      "Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather. Zippered Pockets: 2 Zippered Hand Pockets, 2 Zippered Pockets on Chest (enough to keep cards or keys)and 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design: Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and water,for a comfortable fit. 3 in 1 Detachable Design provide more convenience, you can separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates",
+    category: "women's clothing",
+    image: "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
+    rate: 2.6,
+    quantity: 0,
+    count: 123,
+  },
+  {
+    SKU: 16,
+    name: "Lock and Love Women's Removable Hooded Faux Leather Moto Biker Jacket",
+    price: 29.95,
+    description:
+      "100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON",
+    category: "women's clothing",
+    image: "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg",
+    rate: 2.9,
+    quantity: 0,
+    count: 8,
+  },
+  {
+    SKU: 17,
+    name: "Rain Jacket Women Windbreaker Striped Climbing Raincoats",
+    price: 39.99,
+    description:
+      "Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo it.Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.",
+    category: "women's clothing",
+    image: "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg",
+    rate: 3.8,
+    quantity: 0,
+    count: 123,
+  },
+  {
+    SKU: 18,
+    name: "MBJ Women's Solid Short Sleeve Boat Neck V ",
+    price: 9.85,
+    description:
+      "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem",
+    category: "women's clothing",
+    image: "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg",
+    rate: 4.7,
+    quantity: 0,
+    count: 58,
+  },
+  {
+    SKU: 19,
+    name: "Opna Women's Short Sleeve Moisture",
+    price: 7.95,
+    description:
+      "100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort",
+    category: "women's clothing",
+    image: "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
+    rate: 4.5,
+    quantity: 0,
+    count: 146,
+  },
+  {
+    SKU: 20,
+    name: "DANVOUY Womens T Shirt Casual Cotton Short",
+    price: 12.99,
+    description:
+      "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
+    category: "women's clothing",
+    image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
+    rate: 3.6,
+    quantity: 0,
+    count: 145,
+  },
+];
 /* Declare an empty array named cartArr to hold the items in the cart */
-
-
-// Helper function to get product
-// Most recent product interacted with
-
-
-// Gets the product from the SKU received from the frontend
-
-
+let cartArr = [];
+let totalPaid = 0;
 /* Create a function named addToCart that takes in the product SKU as an argument
   - addToCart should get the correct product based on the SKU
   - addToCart should then increase the product's quantity
   - if the product is not already in the cart, add it to the cart
 */
-
-
+function addToCart(productSKU) {
+  productsArr.forEach((product) => {
+    if (product.SKU === productSKU && !cartArr.includes(product)) {
+      product.quantity += 1;
+      cartArr.push(product);
+      console.log(cartArr);
+    } else if (product.SKU === productSKU && cartArr.includes(product)) {
+      increase(product.SKU);
+      console.log(cartArr);
+    }
+  });
+}
 /* Create a function named increase that takes in the product SKU as an argument
   - increase should get the correct product based on the SKU
   - increase should then increase the product's quantity
 */
-
-
+function increase(productSKU) {
+  productsArr.map((product) => {
+    if (product.SKU === productSKU) {
+      product.quantity += 1;
+    }
+  });
+}
 /* Create a function named decrease that takes in the product SKU as an argument
   - decrease should get the correct product based on the SKU
   - decrease should decrease the quantity of the product
   - if the function decreases the quantity to 0, the product is removed from the cart
 */
-
-
+function decrease(productSKU) {
+  productsArr.map((product) => {
+    if (product.SKU === productSKU) {
+      product.quantity -= 1;
+      if (product.quantity === 0) {
+        remove(product.SKU);
+        cartTotal();
+      }
+    }
+  });
+}
 /* Create a function named remove that takes in the product SKU as an argument
   - remove should get the correct product based on the SKU
   - remove should update the product quantity to 0
   - remove should remove the product from the cart
 */
-
-
-/* When paying, the user should be able to split their payment */
-/* Create a global variable named totalPaid to store the current remaining balance */
-
-
+function remove(productSKU) {
+  for (prod of cartArr) {
+    if (prod.SKU === productSKU) {
+      prod.quantity = 0;
+      console.log(cartArr);
+      let prodIndex = cartArr.indexOf(prod);
+      cartArr.splice(prodIndex, 1);
+      console.log(cartArr);
+    }
+  }
+}
 /* Create a function named cartTotal that has no parameters
   - cartTotal should iterate through the cart to get the total of all products
   - cartTotal should return the sum of the products in the cart
 */
-
+function cartTotal() {
+  let total = 0;
+  cartArr.map((product) => {
+    let prodTotal = product.quantity * product.price;
+    total += prodTotal;
+    console.log(total);
+  });
+  return Number(total);
+}
 
 /* Create a function named pay that takes in an amount as an argument
   - pay will return a negative number if there is a remaining balance
   - pay will return a positive number if money should be returned to customer
 */
-
+function pay(amount) {
+  totalPaid = cartTotal();
+  if (amount - totalPaid > 0) {
+    let sum = (amount - totalPaid).toFixed(2);
+    return Number(sum);
+  } else if (amount - totalPaid < 0) {
+    let sum = (amount - totalPaid).toFixed(2);
+    return Number(sum);
+  }
+}
 
 /* Place stand out suggestions here */
-
-
+/*___________________________________ build an emptyCart function_________________ */
+const emptyCart=()=>{
+  cartArr.forEach(prod=>{
+    prod.quantity=0
+  })
+  cartArr=[]
+}
+/*___________________________________ build a cuurency convertor function_________________ */
+const currency=(curr)=>{
+  console.log(curr)
+  return curr
+}
 
 /* The following is for running unit tests. 
    To fully complete this project, it is expected that all tests pass.
@@ -80,14 +342,14 @@
    npm run test
 */
 module.exports = {
-   productsArr,
-   cartArr,
-   addToCart,
-   increase,
-   decrease,
-   remove,
-   cartTotal,
-   pay, 
-   emptyCart,
-   currency
-}
+  productsArr,
+  cartArr,
+  addToCart,
+  increase,
+  decrease,
+  remove,
+  cartTotal,
+  pay,
+  emptyCart,
+  currency,
+};
